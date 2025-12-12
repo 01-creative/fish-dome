d783a8b4a5cbaff3588a39b8bdf00effb43aa8c1
01-creative <2938698338@qq.com>
2025-12-12 22:33:43 +0800

    重写了结构体，创建了可用的对象池，搭建npc生成框架。

--- Fish-Demo/main.cpp
+++ Fish-Demo/main.cpp
@@ -7,26 +7,43 @@
 #include <math.h>
 #define min(a,b)(((a)<(b))?(a):(b))
 #define max(a,b)(((a)>(b))?(a):(b))
+#define MAX_fish 100
 
 typedef struct fish {
-	double x;
-	double y;
-	double v_x;
-	double v_y;
+	Vector2 xy;
+	Vector2 v_xy;
 	double a;//加速度
-	double size;
-	int aim[1][1];
+	double size;//圆的半径
 	int image_status;
 	int lizixiaoguo;
 	int kinds;//0是玩家，
 }fish;
 
+typedef struct fish_NPC {
+	int active;
+	int aim[7][2];
+	fish fish;
+}fish_NPC;
 
-fish player = { 0,0,0,0,5,40,NULL,0,0,0 };
+typedef struct {
+	fish_NPC fishnpc[MAX_fish];  
+	int used[MAX_fish];       
+	int active_count;         
+} fishPool;
+
+fish player = { {0,0},{0,0},5,40,0,0,0 };
+
 int running = 1;//0是退出，1是正常，2是暂停，3是死亡
 int sleeptime = 100, screen_length_x = 2048, screen_length_y = 1152;
 
 void playermove(fish* player);
+void npc_move(fish_NPC* npc);
+void init_fish_pool(fishPool* pool);
+fish_NPC* get_fish(fishPool* pool);
+void release_fish(fishPool* pool, fish_NPC* fishPtr);
+void update_all_fish(fishPool* pool);
+
+
 int main() {
 
 
@@ -35,6 +52,9 @@ int main() {
 
 	InitWindow(screen_length_x, screen_length_y, "fish game");
 	Texture2D fish_texture = LoadTexture("../img/鱼test.png"), fish_texture2 = LoadTexture("../img/鱼 翻转 test.png");
+
+	fishPool pool;
+	init_fish_pool(&pool);
 	while (running) {
 
 
@@ -42,8 +62,8 @@ int main() {
 
 		BeginDrawing();            // 开始绘制
 		ClearBackground(BLACK);    // 清除上一帧
-		Vector2 test = { player.x,player.y };
-		if (player.v_x > 0)
+		Vector2 test = { player.xy.x,player.xy.y };
+		if (player.v_xy.x > 0)
 		{
 			DrawTextureEx(fish_texture2, test, 0, 0.08, WHITE);
 		}
@@ -72,8 +92,8 @@ void playermove(fish* player) {
 		running = 2;
 		return;
 	}
-	player->v_x *= 0.9;
-	player->v_y *= 0.8;
+	player->v_xy.x *= 0.9;
+	player->v_xy.y *= 0.8;
 	float derta_vx = 0, derta_vy = 0;
 	if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) derta_vx += player->a;
 	if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) derta_vx -= player->a;
@@ -83,32 +103,110 @@ void playermove(fish* player) {
 		derta_vx *= 0.7071f;  // 1/√2
 		derta_vy *= 0.7071f;
 	}
-	player->v_x += derta_vx;
-	player->v_y += derta_vy;
-	player->x += player->v_x;
-	player->y += player->v_y;
-	if (player->x < 0)
+	player->v_xy.x += derta_vx;
+	player->v_xy.y += derta_vy;
+	player->xy.x += player->v_xy.x;
+	player->xy.y += player->v_xy.y;
+	if (player->xy.x < 0)
 	{
-		player->x = 0; player->v_x = 0;
+		player->xy.x = 0; player->v_xy.x = 0;
 	}
-	if (player->x > (screen_length_x - player->size))
+	if (player->xy.x > (screen_length_x - player->size))
 	{
-		player->x = (screen_length_x - player->size); player->v_x = 0.1;
+		player->xy.x = (screen_length_x - player->size); player->v_xy.x = 0.1;
 	}
-	if (player->y < 0)
+	if (player->xy.y < 0)
 	{
-		player->y = 0; player->v_y = 0;
+		player->xy.y = 0; player->v_xy.y = 0;
 	}
-	if (player->y > (screen_length_y - player->size))
+	if (player->xy.y > (screen_length_y - player->size))
 	{
-		player->y = (screen_length_y - player->size); player->v_y = 0.1;
+		player->xy.y = (screen_length_y - player->size); player->v_xy.y = 0.1;
 	}
 }
 
 
 
+void init_fish_pool(fishPool* pool) {
+	for (int i = 0; i < MAX_fish; i++) {  
+		pool->used[i] = 0;  
+		pool->fishnpc[i].active = 0;  
+		pool->fishnpc[i].fish.xy.x = 0; 
+		pool->fishnpc[i].fish.xy.y = 0;  
+		pool->fishnpc[i].active = 0;     
+	}
+	pool->active_count = 0;
+}
 
 
+fish_NPC* get_fish(fishPool* pool) { 
+	for (int i = 0; i < MAX_fish; i++) { 
+		if (pool->used[i] == 0) {
+			pool->used[i] = 1;  
+			pool->active_count++;
+			pool->fishnpc[i].active = 1;  
+			return &pool->fishnpc[i];     
+		}
+	}
+	return 0; 
+}
 
 
+void release_fish(fishPool* pool, fish_NPC* fishPtr) {  
+	int index = fishPtr - pool->fishnpc;  
+	if (index >= 0 && index < MAX_fish) {  
+		pool->used[index] = 0; 
+		pool->active_count--;
+		fishPtr->active = 0;  
+	}
+}
+void update_all_fish(fishPool* pool) {
+	for (int i = 0; i < MAX_fish; i++) {  
+		if (pool->used[i]) {  
+			fish_NPC* fishPtr = &pool->fishnpc[i];  
+
+			fishPtr->fish.xy.x += fishPtr->fish.v_xy.x;
+			fishPtr->fish.xy.y += fishPtr->fish.v_xy.y;
+
+			// 边界检查
+			if (fishPtr->fish.xy.x < 0 || fishPtr->fish.xy.x > screen_length_x ||
+				fishPtr->fish.xy.y < 0 || fishPtr->fish.xy.y > screen_length_y) {
+				release_fish(pool, fishPtr); 
+			}
+		}
+	}
+}
+
+fish_NPC* create_npcfish(fishPool* pool,float x,float y ,int kind) {
+			fish_NPC* newFish = get_fish(pool);
+			int numberofaim = 1;
+		if (newFish == NULL) {
+			return 0; // 池已满，无法创建新鱼
+		}
+		newFish->fish.xy.x = x;
+		newFish->fish.xy.y = y;
+		switch (kind)
+		{
+		case 1: {
+			newFish->fish.v_xy.x = (rand() % 5) - 2;
+			newFish->fish.v_xy.y = (rand() % 5) - 2;
+			newFish->fish.size = 20 + rand() % 30;
+			newFish->fish.a = 2;
+			newFish->fish.kinds = kind;
+			newFish->fish.image_status = kind;
+			for(int i=0;i<numberofaim;i++){
+				newFish->aim[i][0] = rand() % screen_length_x;
+				newFish->aim[i][1] = rand() % screen_length_y;
+			}
+			newFish->aim[numberofaim][0] = max(100*rand() % screen_length_x,3000);
+			newFish->aim[numberofaim][1] = max(100*rand() % screen_length_y,2000);
+		}
+			  break;
+		default:
+			return 0;
+			break;
+		}
+
+	return newFish;
+}
 
