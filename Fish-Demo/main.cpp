20f9d9bc86fd154e77e55143b771e5b13bab07d8
01-creative <2938698338@qq.com>
2025-12-17 14:09:42 +0800

    提供初步的渲染逻辑，生成npc的移动逻辑

--- Fish-Demo/main.cpp
+++ Fish-Demo/main.cpp
@@ -7,7 +7,10 @@
 #include <math.h>
 #define min(a,b)(((a)<(b))?(a):(b))
 #define max(a,b)(((a)>(b))?(a):(b))
-#define MAX_fish 100
+#define Vector2Add(a,b) (Vector2){(a).x+(b).x,(a).y+(b).y}
+#define Vector2Sub(a,b) (Vector2){(a).x-(b).x,(a).y-(b).y}
+#define MAX_fish 20
+
 
 typedef struct fish {
 	Vector2 xy;
@@ -21,7 +24,7 @@ typedef struct fish {
 
 typedef struct fish_NPC {
 	int active;
-	int aim[7][2];
+	Vector2 aim[7];//鱼的移动目标点
 	fish fish;
 }fish_NPC;
 
@@ -31,10 +34,10 @@ typedef struct {
 	int active_count;         
 } fishPool;
 
-fish player = { {0,0},{0,0},5,40,0,0,0 };
+fish player = { {0,0},{0,0},5,60,0,0,0 };
 
 int running = 1;//0是退出，1是正常，2是暂停，3是死亡
-int sleeptime = 100, screen_length_x = 2048, screen_length_y = 1152;
+int sleeptime = 100, screen_length_x = 2048, screen_length_y = 1152,runingtime=0;
 
 void playermove(fish* player);
 void npc_move(fish_NPC* npc);
@@ -42,6 +45,7 @@ void init_fish_pool(fishPool* pool);
 fish_NPC* get_fish(fishPool* pool);
 void release_fish(fishPool* pool, fish_NPC* fishPtr);
 void update_all_fish(fishPool* pool);
+fish_NPC* create_npcfish(fishPool* pool, float x, float y, int kind);
 
 
 int main() {
@@ -52,29 +56,53 @@ int main() {
 
 	InitWindow(screen_length_x, screen_length_y, "fish game");
 	Texture2D fish_texture = LoadTexture("../img/鱼test.png"), fish_texture2 = LoadTexture("../img/鱼 翻转 test.png");
-
+	SetWindowState(FLAG_VSYNC_HINT);
 	fishPool pool;
 	init_fish_pool(&pool);
-	while (running) {
 
-
+	create_npcfish(&pool, 300, 300, 1);
+	while (!WindowShouldClose()) {
+		if(runingtime%71==0)create_npcfish(&pool, rand()%screen_length_x, rand()%screen_length_y, 1);
+		srand(time(NULL)); 
 		playermove(&player);
-
+		update_all_fish(&pool);
 		BeginDrawing();            // 开始绘制
 		ClearBackground(BLACK);    // 清除上一帧
-		Vector2 test = { player.xy.x,player.xy.y };
+		
 		if (player.v_xy.x > 0)
 		{
-			DrawTextureEx(fish_texture2, test, 0, 0.08, WHITE);
+			DrawTextureEx(fish_texture2, player.xy, 0, 0.08, WHITE);
 		}
 		else {
-			DrawTextureEx(fish_texture, test, 0, 0.08, WHITE);
+			DrawTextureEx(fish_texture, player.xy, 0, 0.08, WHITE);
+		}
+		
+		DrawCircle(player.xy.x+player.size/0.7, player.xy.y+ player.size / 1, player.size, RED);
+		//渲染所有NPC鱼
+		for (int i = 0; i < MAX_fish; i++) {
+			if (pool.used[i]) {
+					switch (fish_texture2, pool.fishnpc[i].fish.kinds){
+					case 1:DrawTextureEx(fish_texture2, pool.fishnpc[i].fish.xy, 0, 0.08, WHITE);
+						break;
+
+				}
+			}
 		}
 
-		//DrawCircle(player.x, player.y, player.size, RED);
+
+
+
+
+
+
+
+
+
+
+
 		WaitTime(0.05);
 		EndDrawing();
-
+		runingtime++;
 
 	}
 
@@ -168,6 +196,18 @@ void update_all_fish(fishPool* pool) {
 			fishPtr->fish.xy.x += fishPtr->fish.v_xy.x;
 			fishPtr->fish.xy.y += fishPtr->fish.v_xy.y;
 
+			if (fishPtr->fish.xy.x - fishPtr->aim[0].x <= 1.0 && fishPtr->fish.xy.y - fishPtr->aim[0].y <= 1.0) {
+				for (int p = 0; p < 7; p++) {
+					fishPtr->aim[p] = fishPtr->aim[p + 1];
+			}
+			}
+			fishPtr->aim[6] = { -100,-100};
+			float k = rand() % 40 / 100.0+0.8;
+			Vector2 direction = { fishPtr->aim[0].x- fishPtr->fish.xy.x,fishPtr->aim[0].y - fishPtr->fish.xy.y };
+			fishPtr->fish.v_xy.x += k*(direction.x)* fishPtr->fish.a/pow(((direction.x)* (direction.x)+ (direction.y)* (direction.y)),0.5);
+			fishPtr->fish.v_xy.y += k*(direction.y) * fishPtr->fish.a / pow(((direction.x) * (direction.x) + (direction.y) * (direction.y)), 0.5);
+			fishPtr->fish.v_xy.x *= 0.9;
+			fishPtr->fish.v_xy.y *= 0.8;
 			// 边界检查
 			if (fishPtr->fish.xy.x < 0 || fishPtr->fish.xy.x > screen_length_x ||
 				fishPtr->fish.xy.y < 0 || fishPtr->fish.xy.y > screen_length_y) {
@@ -179,7 +219,7 @@ void update_all_fish(fishPool* pool) {
 
 fish_NPC* create_npcfish(fishPool* pool,float x,float y ,int kind) {
 			fish_NPC* newFish = get_fish(pool);
-			int numberofaim = 1;
+			int numberofaim = 5;
 		if (newFish == NULL) {
 			return 0; // 池已满，无法创建新鱼
 		}
@@ -188,18 +228,18 @@ fish_NPC* create_npcfish(fishPool* pool,float x,float y ,int kind) {
 		switch (kind)
 		{
 		case 1: {
-			newFish->fish.v_xy.x = (rand() % 5) - 2;
-			newFish->fish.v_xy.y = (rand() % 5) - 2;
+			newFish->fish.v_xy.x = (rand() % 7) - 2;
+			newFish->fish.v_xy.y = (rand() % 7) - 2;
 			newFish->fish.size = 20 + rand() % 30;
 			newFish->fish.a = 2;
 			newFish->fish.kinds = kind;
 			newFish->fish.image_status = kind;
 			for(int i=0;i<numberofaim;i++){
-				newFish->aim[i][0] = rand() % screen_length_x;
-				newFish->aim[i][1] = rand() % screen_length_y;
+				newFish->aim[i].x = rand() % screen_length_x*1.5;
+				newFish->aim[i].y = rand() % screen_length_y*1.5;
 			}
-			newFish->aim[numberofaim][0] = max(100*rand() % screen_length_x,3000);
-			newFish->aim[numberofaim][1] = max(100*rand() % screen_length_y,2000);
+			newFish->aim[numberofaim].x = max(100*rand() % screen_length_x,3000);
+			newFish->aim[numberofaim].y = max(100*rand() % screen_length_y,2000);
 		}
 			  break;
 		default:
